8e375b1e374a31ce63ad43126960b7bda5ccf8ea
AlvaroBishop <bishopgamma@gmail.com>
2022-10-26 11:00:56 -0500

    animated container started

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -13,6 +13,7 @@ class AppRoutes {
     MenuOptions(route: 'alert', name: 'Alertas', screen: const AlertScreen(), icon: Icons.add_alert_rounded),
     MenuOptions(route: 'card', name: 'Tarjetas', screen: const CardScreen(), icon: Icons.credit_card_rounded),
     MenuOptions(route: 'avatar', name: 'Circle Avatar', screen: const AvatarScreen(), icon: Icons.supervised_user_circle_outlined),
+    MenuOptions(route: 'animated', name: 'Animated Container', screen: const AnimatedScreen(), icon: Icons.play_circle_outlined),
 
   ];
 
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,8 @@
 
 export 'package:widgets_showdown/screens/alert_screen.dart';
+export 'package:widgets_showdown/screens/animated_screen.dart';
+export 'package:widgets_showdown/screens/avatar_screen.dart';
 export 'package:widgets_showdown/screens/card_screen.dart';
 export 'package:widgets_showdown/screens/home_screen.dart';
 export 'package:widgets_showdown/screens/listview1_screen.dart';
 export 'package:widgets_showdown/screens/listview2_screen.dart';
-export 'package:widgets_showdown/screens/avatar_screen.dart';
