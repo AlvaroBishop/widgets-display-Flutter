2b2a0d4bc867449149b083760bc5f0e0eb3ffc62
AlvaroBishop <bishopgamma@gmail.com>
2022-11-08 20:59:31 -0600

    add widget-form

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -6,16 +6,89 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+    final Map<String, String> formValues = {
+      'first_name': 'Alvaro',
+      'last_name': 'Bishop',
+      'email': 'bishopgamma@gmail.com',
+      'password': '123456',
+      'role': 'Admin'
+    };
+
+    
     return Scaffold(
       appBar: AppBar(title: const Text("Inputs y forms")),
       body: SingleChildScrollView(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(children: const [
-          CustomInputField(labelText: 'Nombre', helperText: 'Nombre del usuario', hintText: 'hint text', counterText: 'counter text'),
-        ]),
+        child: Form(
+          key: myFormKey,
+          child: Column(children: [
+             CustomInputField(
+              labelText: 'Nombre',
+              helperText: 'Nombre del usuario',
+              hintText: 'hint text',
+              counterText: 'counter text',
+              icon: Icons.person,
+              prefixIcon: Icons.perm_contact_cal_rounded,
+              suffixIcon: Icons.next_plan,
+              formProperty: 'first_name',
+              formValues: formValues,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+             CustomInputField(
+                labelText: 'Apellido',
+                helperText: 'Apellido',
+                hintText: 'hint text',
+                counterText: 'counter text',
+                formProperty: 'last_name',
+                formValues: formValues,
+                ),
+            const SizedBox(
+              height: 30,
+            ),
+             CustomInputField(
+              labelText: 'Email',
+              helperText: 'Correo del usuario',
+              hintText: 'hint text',
+              counterText: 'counter text',
+              keyboardType: TextInputType.emailAddress,
+              formProperty: 'email',
+              formValues: formValues,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+             CustomInputField(
+                labelText: 'Contraseña',
+                helperText: 'Contraseña del usuario',
+                hintText: 'hint text',
+                counterText: 'counter text',
+                obscureText: true,
+                formProperty: 'password',
+                formValues: formValues,
+             ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  if (!myFormKey.currentState!.validate()) {
+                    print('Formulario no valido');
+                    return;
+                  }
+                  print(formValues);
+                },
+                child: const SizedBox(
+                  width: double.infinity,
+                  child: Center(child: Text('Guardar')),
+                ))
+          ]),
+        ),
       )),
     );
   }
 }
-
--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -8,45 +8,58 @@ class CustomInputField extends StatelessWidget {
   final IconData? icon;
   final IconData? prefixIcon;
   final IconData? suffixIcon;
-  
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  final String formProperty;
+  final Map<String, String> formValues;
+
   const CustomInputField({
-    Key? key, 
-    this.counterText, 
-    this.helperText, 
-    this.hintText, 
-    this.labelText, this.icon, this.prefixIcon, this.suffixIcon,
+    Key? key,
+    this.counterText,
+    this.helperText,
+    this.hintText,
+    this.labelText,
+    this.icon,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.keyboardType,
+    this.obscureText = false, 
+    required this.formProperty, 
+    required this.formValues,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-        autofocus: true,
-        initialValue: '',
-        textCapitalization: TextCapitalization.words,
-        onChanged: (value) => print('value: $value'),
-        validator: (value) {
-          //valida el formulario
-          if (value == null) return 'Este campo es requerido';
-          return value.length < 3 ? 'Minimo de 3 letras' : null;
-        },
-        // determina cuando validar el formulario
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        decoration: InputDecoration(
-            counterText: counterText,
-            helperText: helperText,
-            hintText: hintText,
-            labelText: labelText,
-            icon: icon == null ? null :  Icon( icon ),
-            prefixIcon: prefixIcon == null ? null :  Icon( prefixIcon ),
-            suffixIcon: suffixIcon == null ? null :  Icon( suffixIcon ),
-            // focusedBorder: OutlineInputBorder(
-            //     borderSide: BorderSide(color: Colors.green)),
-            // border: OutlineInputBorder(
-            //   borderRadius: BorderRadius.only(
-            //       bottomLeft: Radius.circular(10),
-            //       topRight: Radius.circular(10)),
-            ),
-            
-            );
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      autofocus: true,
+      initialValue: '',
+      textCapitalization: TextCapitalization.words,
+      onChanged: (value) => formValues[formProperty] = value,
+      validator: (value) {
+        //valida el formulario
+        if (value == null) return 'Este campo es requerido';
+        return value.length < 3 ? 'Minimo de 3 letras' : null;
+      },
+      // determina cuando validar el formulario
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        counterText: counterText,
+        helperText: helperText,
+        hintText: hintText,
+        labelText: labelText,
+        icon: icon == null ? null : Icon(icon),
+        prefixIcon: prefixIcon == null ? null : Icon(prefixIcon),
+        suffixIcon: suffixIcon == null ? null : Icon(suffixIcon),
+        // focusedBorder: OutlineInputBorder(
+        //     borderSide: BorderSide(color: Colors.green)),
+        // border: OutlineInputBorder(
+        //   borderRadius: BorderRadius.only(
+        //       bottomLeft: Radius.circular(10),
+        //       topRight: Radius.circular(10)),
+      ),
+    );
   }
 }
